6d08798ca4c35e8d8b6cd94b0d9fab8cd79e465b
Mohamed-Mohamed-Afifi <mohamed.org050@gmail.com>
2022-03-24 22:15:11 +0200

    build files

--- src/Components/header.js
+++ src/Components/header.js
@@ -2,10 +2,11 @@ import React,{Fragment} from "react";
 import { Container,Navbar,Nav,Form,FormControl,Button} from "react-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import "../componentStyle/nav.scss";
 const Navv=()=>{
     return(
         <Fragment>
-        <Navbar bg="light" expand="lg">
+        <Navbar expand="lg">
            <Container>
            <Navbar.Brand href="#">
            <img src="https://www.doorwaysva.org/wp-content/uploads/2019/06/amazon-logo.png" width ="100" heigth="100" className="d-inline-block align-top" alt="Amazon"/>
--- src/index.js
+++ src/index.js
@@ -2,18 +2,13 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDom from "react-dom";
 import Footer from "./Components/Footer";
-
-import { Last } from "./Components/Nav";
-
 import Cards from "./Components/Cards";
-
 import Navv from "./Components/header";
 /* Data*/
 
 ReactDom.render(
   <React.StrictMode>
     <Navv/>
-    <Last />
     <Cards/>
     <Footer />
   </React.StrictMode>,
